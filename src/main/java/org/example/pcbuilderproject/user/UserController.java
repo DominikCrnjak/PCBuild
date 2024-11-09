f197328cf687f87ce97897efa659d0c5db0eea0d
Dominik <dominik.crnjak@gmail.com>
2024-11-09 19:54:14 +0100

    Users controller

---
+++ src/main/java/org/example/pcbuilderproject/user/UserController.java
@@ -0,0 +1,40 @@
+package org.example.pcbuilderproject.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Endpoint za dohvaćanje trenutnog korisnika
+    @GetMapping("/me")
+    public ResponseEntity<Object> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body("User is not authenticated");
+        }
+
+        Object currentUser = authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+    // Endpoint za dohvaćanje svih korisnika
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
---
+++ src/main/java/org/example/pcbuilderproject/user/UserService.java
@@ -0,0 +1,24 @@
+package org.example.pcbuilderproject.user;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
